9d125f5c10f807f08ec2a44ec07bb1baeb3d1b40
dafifynn <slimbieber@gmail.com>
2023-03-31 19:01:13 -0300

    make fuzzer for ligistic regression

--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -1,18 +1,24 @@
 #[macro_use]
 extern crate honggfuzz;
+
 extern crate cpython;
 
-use cpython::{PyObject, PyResult, Python};
+use cpython::{PyObject, PyResult, PyLong, PyTuple, Python};
 
 fn main() {
     loop {
-        fuzz!(|data1: u64, data2: u64| {
+        fuzz!(|data: (i64, i64)| {
 
             let gil = Python::acquire_gil();
             let py = gil.python();
-            let script = py.import("logistic_regression")?;
-            let result = script.call(py, "make_classification", (input,), None).unwrap();
+            let script = py.import("logistic_regression").unwrap();
+            let high_i64 = data.0;
+            let low_i64 = data.1;
+            let py_high = py.eval(&format!("int({})", high_i64), None, None).unwrap();
+            let py_low = py.eval(&format!("int({})", low_i64), None, None).unwrap();
+
+            let args = PyTuple::new(py, &[py_high, py_low]);
+            let result = script.call(py, "compare_concrete_scikit_predictions", args, None).unwrap();
         });
     }
 }
-
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,46 +1,35 @@
-rom sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression as lr_sklearn
 from concrete.ml.sklearn import LogisticRegression as lr_concrete
 
-# Lets create a synthetic data-set
-x, y = make_classification(n_samples=100, class_sep=2, n_features=30, random_state=42)
+def compare_concrete_scikit_predictions(input_test):
 
-# Split the data-set into a train and test set
-X_train, X_test, y_train, y_test = train_test_split(
-            x, y, test_size=0.2, random_state=42
-            )
+    # Dataset to train
+    x, y = make_classification(n_samples=100, class_sep=2, n_features=30, random_state=42)
 
-# Now we train in the clear and quantize the weights
-concrete_model = lr_concrete()
-concrete_model.fit(X_train, y_train)
+    # Split the data-set into a train and test set,
+    # each set is split into input and result.
+    input_train, input_test, result_train, _ = train_test_split(
+        x, y, test_size=0.2, random_state=42
+    )
 
-# We can simulate the predictions in the clear
-y_pred_clear = concrete_model.predict(X_test)
+    # Start the concrete-ml logistic regression model
+    concrete_model = lr_concrete()
+    # Train
+    concrete_model.fit(input_train, result_train)
 
-# We then compile on a representative set
-concrete_model.compile(X_train)
+    # Compile FHE
+    concrete_model.compile(input_train)
 
-# Finally we run the inference on encrypted inputs
-y_pred_fhe = concrete_model.predict(X_test, execute_in_fhe=True)
+    # Run the inference on encrypted inputs
+    fhe_pred = concrete_model.predict(input_test, execute_in_fhe=True)
 
-print("concrete-ml")
-print("In clear  :", y_pred_clear)
-print("In FHE    :", y_pred_fhe)
-print(f"Similarity: {int((y_pred_fhe == y_pred_clear).mean()*100)}%")
+    # Start the sklearn logistic regression model
+    scikit_model = lr_sklearn()
+    # Train
+    scikit_model.fit(input_train, result_train)
 
-# Output:
-# In clear  : [0 0 0 0 1 0 1 0 1 1 0 0 1 0 0 1 1 1 0 0]
-# In FHE    : [0 0 0 0 1 0 1 0 1 1 0 0 1 0 0 1 1 1 0 0]
-# Similarity: 100%
-#X_train, X_test, y_train, y_test = train_test_split(
-#            x, y, test_size=0.2, random_state=42
-#            )
-scikit_model = lr_sklearn()
-scikit_model.fit(X_train, y_train)
-sk_y_pred_clear = scikit_model.predict(X_test)
-sk_y_pred_fhe = scikit_model.predict(X_test)
-print("\nscikit-learn")
-print("In clear  :", sk_y_pred_clear)
-print("In FHE    :", sk_y_pred_fhe)
-print(f"Similarity: {int((y_pred_fhe == y_pred_clear).mean()*100)}%")
+    # Get scikit prediction
+    prediction = scikit_model.predict(input_test)
+
+    assert((fhe_pred == prediction).all())
